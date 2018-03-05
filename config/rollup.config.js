d96fac37926b97134089babbba3b5a7958892828
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-03-05 14:58:23 -0800

    Formatting

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -2,11 +2,11 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 /**
- * @param {boolean} esmodules 
+ * @param {boolean} esmodules
  * @returns {Array<OutputConfig>} Rollup configurations for output.
  */
 function outputConfiguration(esmodules) {
-  const basePath = `build/${esmodules === true && 'esmodules' || ''}`;
+  const basePath = `build/${(esmodules === true && 'esmodules') || ''}`;
 
   return [
     {
@@ -23,49 +23,44 @@ function outputConfiguration(esmodules) {
       file: `${basePath}/index.js`,
       format: 'iife',
       sourcemap: true,
-      name: 'WorkerDom'
-    }
-  ]
-};
+      name: 'WorkerDom',
+    },
+  ];
+}
 
 /**
- * 
- * @param {boolean} esmodules 
+ *
+ * @param {boolean} esmodules
  * @returns {Object} Babel configuration for output.
  */
 function babelConfiguration(esmodules) {
-  const targets = esmodules === true && { esmodules: true } || { browsers: ['last 2 versions', 'ie >= 11', 'safari >= 7'] };
+  const targets = (esmodules === true && { esmodules: true }) || { browsers: ['last 2 versions', 'ie >= 11', 'safari >= 7'] };
 
   return {
     exclude: 'node_modules/**',
     presets: [
       [
-        '@babel/env', {
+        '@babel/env',
+        {
           targets: targets,
           loose: true,
-          modules: false
-        }
+          modules: false,
+        },
       ],
     ],
-    plugins: [
-      ['@babel/plugin-proposal-object-rest-spread'],
-      ['@babel/proposal-class-properties'],
-    ]
+    plugins: [['@babel/plugin-proposal-object-rest-spread'], ['@babel/proposal-class-properties']],
   };
-};
+}
 
-export default [{
-	input: 'src/output/index.js',
-	output: outputConfiguration(false),
-	plugins: [
-		babel(babelConfiguration(false)),
-		nodeResolve({ jsnext:true }),
-	]
-}, {
-  input: 'src/output/index.js',
-	output: outputConfiguration(true),
-	plugins: [
-		babel(babelConfiguration(true)),
-		nodeResolve({ jsnext:true }),
-	]
-}];
+export default [
+  {
+    input: 'src/output/index.js',
+    output: outputConfiguration(false),
+    plugins: [babel(babelConfiguration(false)), nodeResolve({ jsnext: true })],
+  },
+  {
+    input: 'src/output/index.js',
+    output: outputConfiguration(true),
+    plugins: [babel(babelConfiguration(true)), nodeResolve({ jsnext: true })],
+  },
+];
