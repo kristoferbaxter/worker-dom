df3f4b490b44c1a29cfbe123fbcc5f39bb47ac64
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-03-07 11:57:52 -0800

    Better JSDoc

--- src/Element.ts
+++ src/Element.ts
@@ -2,11 +2,9 @@ import { Node, NodeType } from './Node';
 
 export class Element extends Node {
   /**
-   * Element.children() is supposed to be supplied by a ParentNode mixin.
-   * Intentionally implemewnted only on Element and extensions of it here.
-   *
-   * Returns a live HTMLCollection containing all of the Element objects
-   * that are children of this ParentNode, omitting all of its non-element nodes.
+   * Getter returning children of an Element that are Elements themselves.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/children
+   * @returns Element objects that are children of this ParentNode, omitting all of its non-element nodes.
    */
   get children(): Element[] {
     return this.childNodes.filter(node => node.nodeType === NodeType.ELEMENT_NODE) as Element[];
