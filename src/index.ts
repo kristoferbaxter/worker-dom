113067641da15e9cc4d3b9b852386f61a0e4b194
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-03-21 14:14:02 -0700

    Ready for review

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,4 @@ import { Element } from './Element';
 import { Text } from './Text';
 import { Event } from './Event';
 
-// interface window extends Window {
-//   dom: {
-//     Node: typeof Node,
-//     Element: typeof Element,
-//     Text: typeof Text,
-//     Event: typeof Event,
-//   }
-// }
-
-// if (typeof window !== 'undefined') {
-//   (window as window).dom = { Node, Element, Text, Event };
-// }
-
 export { Node, Element, Text, Event };
