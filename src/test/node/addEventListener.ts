9b6def0b9d5f852df6877aacaf65cb5027803320
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-04-23 15:18:13 -0700

    Tests for addEventListener

---
+++ src/test/node/addEventListener.ts
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import test from 'ava';
+import { Node, NodeType } from '../../worker-thread/Node';
+
+test.beforeEach(t => {
+  t.context = {
+    node: new Node(NodeType.ELEMENT_NODE, 'div'),
+  };
+});
+
+test('event handlers are undefined by default', t => {
+  const { node } = t.context;
+
+  t.is(node._handlers_['click'], undefined);
+});
+
+test('adding an event listener increases total registered events from 0 to 1 on a Node', t => {
+  const { node } = t.context;
+  const callback = () => undefined;
+
+  node.addEventListener('click', callback);
+  t.is(node._handlers_['click'].length, 1);
+  t.is(node._handlers_['click'][0], callback);
+});
+
+test('adding a second event listener increases total registered events from 0 to 1 on a Node', t => {
+  const { node } = t.context;
+  const callback = () => undefined;
+  const secondcallback = () => undefined;
+
+  node.addEventListener('click', callback);
+  node.addEventListener('click', secondcallback);
+  t.is(node._handlers_['click'].length, 2);
+  t.is(node._handlers_['click'][0], callback);
+  t.is(node._handlers_['click'][1], secondcallback);
+});
