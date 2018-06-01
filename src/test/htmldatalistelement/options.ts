68abf0ab9de02649c535023e2740406d3777da66
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-06-01 14:27:47 -0700

    Tests for HTMLDataListElement

---
+++ src/test/htmldatalistelement/options.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import test from 'ava';
+import { Node, NodeType } from '../../worker-thread/Node';
+import { Element } from '../../worker-thread/Element';
+import { HTMLDataListElement } from '../../worker-thread/HTMLDataListElement';
+
+test.beforeEach(t => {
+  t.context = {
+    node: new HTMLDataListElement(NodeType.ELEMENT_NODE, 'datalist', null),
+    option: new Element(NodeType.ELEMENT_NODE, 'option', null),
+    optionTwo: new Element(NodeType.ELEMENT_NODE, 'option', null),
+    text: new Node(NodeType.TEXT_NODE, '#text'),
+    invalidElement: new Element(NodeType.ELEMENT_NODE, 'div', null),
+  };
+});
+
+test('options should be an empty array when there are no childNodes', t => {
+  const { node } = t.context as { node: HTMLDataListElement };
+
+  t.is(node.options.length, 0);
+  t.deepEqual(node.options, []);
+});
+
+test('options should contain all childNodes when all have the correct node name', t => {
+  const { node, option, optionTwo } = t.context as { node: HTMLDataListElement; option: Element; optionTwo: Element };
+
+  node.appendChild(option);
+  node.appendChild(optionTwo);
+  t.is(node.options.length, 2);
+  t.deepEqual(node.options, [option, optionTwo]);
+});
+
+test('options should contain only childNodes of the correct node name', t => {
+  const { node, option, optionTwo, text, invalidElement } = t.context as {
+    node: HTMLDataListElement;
+    option: Element;
+    optionTwo: Node;
+    text: Node;
+    invalidElement: Element;
+  };
+
+  node.appendChild(option);
+  node.appendChild(optionTwo);
+  node.appendChild(text);
+  node.appendChild(invalidElement);
+  t.is(node.options.length, 2);
+  t.deepEqual(node.options, [option, optionTwo]);
+});
+
+// test('children should be an empty array when there are no childNodes of NodeType.ELEMENT_NODE', t => {
+//   const { node, childTwo } = t.context as { node: Element; childTwo: Node };
+
+//   node.appendChild(childTwo);
+//   t.is(node.children.length, 0);
+//   t.deepEqual(node.children, []);
+// });
